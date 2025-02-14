1afc5e1c01a087d1b925fb3143efb6311f24cc43
sugasaki <sugasaki@gmail.com>
2025-02-14 13:20:37 +0900

    ヘッダーのタイトルを「GPX Viewer」に変更し、CSSのスタイルを微調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <h1>React Map GL と GPXルート表示</h1>
+      <h2>GPX Viewer</h2>
     </header>
   );
 };
